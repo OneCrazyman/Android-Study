8e03a5ed730e2e8130989052069719f85fe56812
OneCrazyman <cjw7155@naver.com>
2023-04-01 04:52:04 +0900

    add: TimerApp

--- CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
+++ CounterApp/app/src/main/java/com/example/counterapp/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnDown;
     Button btnUp;
+    Button btnInputCheck;
     TextView tvNum;
     Integer count;
+    Integer UnitNum;
+    EditText etInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,15 @@ public class MainActivity extends AppCompatActivity {
     public void IntializeView() {
         btnDown = (Button) findViewById(R.id.buttonDown);
         btnUp = (Button) findViewById(R.id.buttonUp);
+        btnInputCheck = (Button) findViewById(R.id.buttonInput);
         tvNum = (TextView) findViewById(R.id.tvNum);
+        etInput = (EditText) findViewById(R.id.etInput);
+
         count = 0;
+        UnitNum = 1;
+
+        tvNum.setText(Integer.toString(count));
+        etInput.setText(Integer.toString(UnitNum));
     }
 
     //익명클래스
@@ -35,20 +47,34 @@ public class MainActivity extends AppCompatActivity {
         btnDown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count--;
+                Integer tmp = count-UnitNum;
+                if (tmp >= 0) {
+                    count=tmp;
+                    Update();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "0미만으로 내려갈수없습니다.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count++;
+                count+=UnitNum;
+                Update();
             }
         });
-        Update();
+        btnInputCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UnitNum = Integer.parseInt(String.valueOf(etInput.getText()));
+            }
+        });
+
     }
 
     //업데이트
     public void Update(){
-        tvNum.setText(count);
+        tvNum.setText(Integer.toString(count));
     }
 }
